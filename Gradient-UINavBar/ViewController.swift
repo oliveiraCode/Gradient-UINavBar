0abda43490c98ac1d577346882266636de9a4c55
Leandro Oliveira <leandro.oliveira@live.com>
2019-02-08 11:51:33 -0500

    Default Project

---
+++ Gradient-UINavBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gradient-UINavBar
+//
+//  Created by Leandro Oliveira on 2019-02-08.
+//  Copyright © 2019 OliveiraCode Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
